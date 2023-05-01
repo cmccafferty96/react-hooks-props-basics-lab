3f40a87defcd634ece6b47661b23838487caa6c9
+ <+>
2023-05-01 15:58:08 -0400

    completed react hooks props lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,17 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  let para;
+  if(props.bio){
+    para = <p>{props.bio}</p>
+  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {para}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin} />
     </div>
   );
 }
